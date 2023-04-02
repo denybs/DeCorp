530ac9e3cd6eb1840fa2bc4d87aa860767f0dde0
Dény Baskhanov <deny.baskhanov@gmail.com>
2023-04-02 13:01:35 +0200

    ajout de la metric euclidienne a l'algorithme

--- face/reco/knn/knnModel.py
+++ face/reco/knn/knnModel.py
@@ -1,14 +1,20 @@
+from math import sqrt
 """
 il faut que :
 -le dataset soit une liste des lignes de liste des colonnes (features) de taille (nLignes,mColones)
 -les targets soient dans une liste séparé 
 -les donnes de la classe inconnue doivent etre sous forme d'une liste contenant une ligne et les colonnes de taille (1,mColonnes) 
 """
-def calculDistance(firstElement, secondElement):#firstelElement est la liste de features du premier element et same for secondElement
+def calculDistance(firstElement, secondElement,metric):#firstelElement est la liste de features du premier element et same for secondElement
     distance = 0
-    for value1, value2 in zip(firstElement,secondElement):
-        distance+=abs(value1-value2) #metrique de Manathan
-    return distance
+    if metric == 'manhattan':
+        for value1, value2 in zip(firstElement,secondElement):
+            distance+=abs(value1-value2) #metrique de Manathan
+        return distance
+    elif metric == 'euclidean':
+        for value1, value2 in zip(firstElement,secondElement):
+            distance+=(value1-value2)**2 #metrique euclidienne
+        return sqrt(distance)
 def determineClass(kNearest,labels):
     classesIndex=[]
     smallestDistance = min(kNearest)
@@ -38,7 +44,7 @@ def determineClass(kNearest,labels):
 def knn(classeInconnue,features,labels,k): #on considere que classeInconnue et les colonnes du dataset sont les memes tailles
     kNearest = []
     for feature in features:
-        distance = calculDistance(classeInconnue,feature)
+        distance = calculDistance(classeInconnue,feature,metric = "manhattan")
         kNearest.append((distance,features.index(feature)))
     kNearest.sort(reverse= True)
     kNearest = kNearest[len(kNearest)-k:]
